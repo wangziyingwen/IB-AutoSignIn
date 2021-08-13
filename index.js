12ddbf35a07792f80b69f0c1d1267c15f71a9d7d
wangziyingwen <wz.lxh@outlook.com>
2021-08-14 01:04:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const $http = require("axios");
     $http.defaults.baseURL = process.env.HOST
     const username = process.env.USERNAME
     const password = process.env.PASSWORD
+    const tgtoken = process.env.TGTOKEN
+    const chatid = process.env.TGCHATID
     let cookie = {}
     function setHeaders(arr,raw) {
       arr.forEach(item=>{
@@ -39,18 +41,43 @@ const $http = require("axios");
     res = await $http.get('/',{
       'headers':setHeaders(res.headers['set-cookie'] || [],cookie)
     })
+    let signState = new Date(new Date().getTime() + 8 * 3600 * 1000).toJSON().substr(0, 19).replace('T', ' ').replace(/-/g, '.')
     if(res.data.includes(username)){
       console.log('登录成功')
-      $http.get(`/plugin.php?id=k_misign:sign&operation=qiandao&formhash=${hashparse.exec(res.data)[1]}&format=empty&inajax=1&ajaxtarget=JD_sign`,{
+      await $http.get(`/plugin.php?id=k_misign:sign&operation=qiandao&formhash=${hashparse.exec(res.data)[1]}&format=empty&inajax=1&ajaxtarget=JD_sign`,{
         'headers':setHeaders(res.headers['set-cookie'] || [],cookie)
-      }).then(()=>{
-        console.log('  > 签到成功')
+      }).then(async ()=>{
+        await $http.get('/',{
+          'headers':setHeaders([],cookie)
+        }).then(res=>{
+          if(res.data.includes(username) && !res.data.includes('您今天还没有签到')){
+            signState +='\n  > 签到成功'
+            console.log('  > 签到成功')
+          }else{
+            signState +='\n  > 已签到，未知'
+            console.log('  > 已签到，未知')
+          }
+        }).catch(()=>{
+          signState +='\n  > 已签到，未知'
+          console.log('  > 已签到，未知')
+        })
       }).catch(()=>{
+        signState +='\n  > 签到失败'
         console.log('  > 签到失败')
       })
     }else{
+      signState +='\n  > 登录失败'
       console.log('登录失败')
     }
+    $http.post(`https://api.telegram.org/bot${tgtoken}/sendMessage`,{
+      'chat_id':chatid,
+      'text':signState,
+      'parse_mode':'HTML'
+    },{
+      headers:{
+        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
+      }
+    })
 })()
 
 
