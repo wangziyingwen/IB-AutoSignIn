f2138727c451396a0ec7d60890a20175c0223ca3
wangziyingwen <wz.lxh@outlook.com>
2021-08-14 01:12:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -69,17 +69,16 @@ const $http = require("axios");
       signState +='\n  > 登录失败'
       console.log('登录失败')
     }
-    $http.post(`https://api.telegram.org/bot${tgtoken}/sendMessage`,{
+    $http.post(`https://api.telegram.org/bot${tgtoken}/sendMessage`,JSON.stringify({
       'chat_id':chatid,
       'text':signState,
       'parse_mode':'HTML'
-    },{
+    }),{
       headers:{
+        'Content-Type': 'application/json',
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
       }
-    }).catch(
-      console.log(1)
-    )
+    })
 })()
 
 
