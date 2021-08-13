5e684196e72f66ab82f70fa0d7f33b252304e5c0
wangziyingwen <wz.lxh@outlook.com>
2021-08-14 01:09:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,9 @@ const $http = require("axios");
       headers:{
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
       }
-    })
+    }).catch(
+      console.log(1)
+    )
 })()
 
 
