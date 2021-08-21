bfec7d3e92e817e5bb46ceaa3197afa7e7dd6da4
wangziyingwen <wz.lxh@outlook.com>
2021-08-21 21:21:12 +0800

    Update soldout.js

--- soldout.js
+++ soldout.js
@@ -8,6 +8,7 @@ $http.get('https://www.hmv.co.jp/search/adv_1/category_3/keyword_No+More+Heroes+
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
       }
 }).then(res=>{
+        console.log(res.data)
         if(!res.data.includes('販売終了')){
             if(tgbot[0] && tgbot[1]){
                 $http.post(`https://api.telegram.org/bot${tgbot[1]}/sendMessage`,{
@@ -31,6 +32,7 @@ $http.get('https://www.hmv.co.jp/basket/updatewishlist/method_type/CARTALL/?sku2
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
       }
 }).then(res=>{
+    console.log(res.data)
     if(!res.data.includes('は完売しました')){
         if(tgbot[0] && tgbot[1]){
             $http.post(`https://api.telegram.org/bot${tgbot[1]}/sendMessage`,{
