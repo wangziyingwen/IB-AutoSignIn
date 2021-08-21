0ebc544b8581ae4d01f87f8c8638b277b65ceeab
wangziyingwen <wz.lxh@outlook.com>
2021-08-21 21:13:56 +0800

    Update soldout.js

--- soldout.js
+++ soldout.js
@@ -4,6 +4,7 @@ const tgbot = (process.env.TGBOT || '').split('&&',2)
 
 $http.get('https://www.hmv.co.jp/search/adv_1/category_3/keyword_No+More+Heroes+3++Nintendo+Switch+/target_GAMES/type_sr/',{
     headers:{
+        'Accept-Language': 'zh-CN,zh-HK;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6',
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
       }
 }).then(res=>{
@@ -26,6 +27,7 @@ $http.get('https://www.hmv.co.jp/search/adv_1/category_3/keyword_No+More+Heroes+
 
 $http.get('https://www.hmv.co.jp/basket/updatewishlist/method_type/CARTALL/?sku2%5B%5D=11776374',{
     headers:{
+        'Accept-Language': 'zh-CN,zh-HK;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6',
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
       }
 }).then(res=>{
